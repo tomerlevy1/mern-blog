7a7a4ed14b9494f690aea845bab72341b7745825
Tomer <tolevy@outbrain.com>
2024-06-06 22:13:17 +0300

    feat(server): init mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3001;
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+  });
 
 // app.get("/", (req, res) => res.send("Hello World!"));
 
