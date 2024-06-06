3a2b77535f72103d86fb5be1f29677fbf96f43a4
Tomer <tolevy@outbrain.com>
2024-06-06 22:51:40 +0300

    feat(server): add user route

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { configDotenv } from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import userRoute from './routes/user.route.js';
 
 const app = express();
 const port = 3001;
 
+configDotenv();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB!");
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
-    console.log("Error connecting to MongoDB: ", err);
+    console.log('Error connecting to MongoDB: ', err);
   });
-
-// app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
 });
+
+app.use('/api/user', userRoute);
